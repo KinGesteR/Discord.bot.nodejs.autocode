47d86bbd5966c54787569e2514766f4b873d846a
KinGesteR <96717730+KinGesteR@users.noreply.github.com>
2021-12-27 21:24:28 +0330

    Update help.js

--- functions/events/discord/message/help.js
+++ functions/events/discord/message/help.js
@@ -2,6 +2,7 @@ const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 let message = context.params.event.content;
 
+//Bot prefix is [ ? ]
 if (message.startsWith(`?help`)) {
   await lib.discord.channels['@0.0.6'].messages.create({
     channel_id: `${context.params.event.channel_id}`,
